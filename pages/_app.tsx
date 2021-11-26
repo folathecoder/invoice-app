fccb623e2243296b0471a12204be63685fda2e1a
folathecoder <officialakinloyefolarin@gmail.com>
2021-11-27 00:49:51 +0100

    App Layout ==== Created & Adjusted Layout to Accommodate SideNav

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,26 @@
 import type { AppProps } from "next/app";
 import { GlobalStyles } from "styles/globalStyles";
 import MobileNav from "components/sections/header/mobileNav/mobileNav";
+import {
+  TemplateWrap,
+  SideNavWrap,
+  MainContent,
+} from "styles/appLayoutStyles";
+import DesktopNav from "components/sections/header/desktopNav/desktopNav";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyles />
       <MobileNav />
-      <Component {...pageProps} />
+      <TemplateWrap>
+        <SideNavWrap>
+          <DesktopNav />
+        </SideNavWrap>
+        <MainContent>
+          <Component {...pageProps} />
+        </MainContent>
+      </TemplateWrap>
     </>
   );
 }
